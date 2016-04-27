d3baeb40d6602aaa9efc42d5c9cae2201caf8752
swoskowiak <swoskowiak@gmail.com>
2016-04-27 20:08:29 +0000

    initial commit

---
+++ actions/image.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var getPixel = require('get-pixels'),
+  prevUrl, // Previous url we loaded
+  prevPixels; // Previous store of pixels
+
+/**
+ * Builds frame data out of ndArray of pixels
+ * @param  {ndArray} pixels   ndArray of pixels (https://www.npmjs.com/package/ndarray)
+ * @param  {Number} ledCount  the number of leds this strip uses
+ * @param  {Number} yOffset   the height we want to start slicing our frames at (defaults to 0)
+ * @param  {Number} ledHeight the height of our ledStrip (defaults to 1)
+ * @return {Array}           Frame data to animate through our led strip
+ */
+function buildFramesFromPixels(pixels, ledCount, yOffset, ledHeight) {
+  var i, j, k, l,
+  curFrame, color, frames = [],
+  maxWidth, maxHeight;
+
+  console.log('Building frame map...');
+
+  // Make sure we attempt to grab valid values (don't step out of image size bounds)
+  maxWidth = Math.min(ledCount, pixels.shape[1]);
+  maxHeight = Math.min(ledHeight, pixels.shape[2]);
+
+  console.log('Frames: ' + pixels.shape[0] + ' - Dimensions: ' + pixels.shape[1] + 'x' + pixels.shape[2]);
+  // For an animated gif in get-pixels it returns the following:
+  // shape[0] = number of frames (i)
+  // shape[1] = frame width (in pixels) (j)
+  // shape[2] = frame height (in pixels) (k)
+  // shape[3] = RGB(A) value of pixel at given offset (l)
+  for (i = 0; i < pixels.shape[0]; i++) {
+    curFrame = [];
+    for (j = 0; j < maxWidth; j++) {
+      for (k = yOffset; k < (maxHeight + yOffset); k++) {
+        color = [];
+        for (l = 0; l < 3; l++) {
+          color.push(pixels.get(i, j, k, l));
+        }
+        curFrame.push(color);
+      }
+    }
+    frames.push(curFrame);
+  }
+  console.log('Frame map complete.');
+  return frames;
+}
+
+/**
+ * Takes an imgUrl for a gif and converts it to frame data
+ * @param  {String} imgUrl    url to a gif image
+ * @param  {Number} ledCount  the number of leds this strip uses
+ * @param  {Number} yOffset   the height we want to start slicing our frames at (defaults to 0)
+ * @param  {Number} ledHeight the height of our ledStrip (defaults to 1)
+ * @return {Promise}           returns promise that resolves once remote img is loaded and frame data is built
+ */
+module.exports = function (imgUrl, ledCount, yOffset, ledHeight) {
+
+  ledHeight = ledHeight || 1;
+  yOffset = yOffset || 0;
+
+  // Returns a promise
+  return new Promise(function (resolve, reject) {
+    if ((imgUrl === prevUrl) && prevPixels) {
+      console.log('Using existing pixel data for image: ' + imgUrl);
+      resolve(buildFramesFromPixels(prevPixels, ledCount, yOffset, ledHeight));
+    } else {
+      console.log('\nDownloading image: ' + imgUrl + ' ....\n');
+      // Call get-pixel
+      getPixel(imgUrl, function (err, pixels) {
+        if (err) {
+          console.log('Image failed to load from: ' + imgUrl);
+          resolve(false);
+          return false;
+        }
+        prevUrl = imgUrl;
+        prevPixels = pixels;
+        resolve(buildFramesFromPixels(pixels, ledCount, yOffset, ledHeight));
+      });
+    }
+  });
+};
---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var awsIot = require('aws-iot-device-sdk'),
+  leds = require('rpi-ws2801'),
+  _ = require('lodash'),
+  device,
+  iotSettings = {
+    host: 'AB9CT7LOKIG9O.iot.us-east-1.amazonaws.com',
+    port: 8883,
+    clientId: 'blinkie',
+    thingName: 'blinkie',
+    caCert: 'root-CA.crt',
+    clientCert: '150480e48f-certificate.pem.crt',
+    privateKey: '150480e48f-private.pem.key'
+  },
+  frameBuilder = require('./actions/image'),
+  ledCount = 160,
+  curInterval = '';
+
+console.log('Connecting to Amazon IoT...');
+device = awsIot.device(iotSettings);
+// On Amazon IoT connection
+device.on('connect', function () {
+  console.log('Amazon IoT connect Success');
+  // Subscripe to Blinkie related topics
+  device.subscribe('blinkie:fill');
+  device.subscribe('blinkie:gif');
+  device.subscribe('blinkie:clear');
+
+  console.log('Awaiting messages...');
+  // Connect LEDS
+  leds.connect(ledCount);
+  leds.clear();
+});
+
+/**
+ * Simple reset function to clear existing intervals and turn off leds
+ */
+function reset() {
+  clearInterval(curInterval);
+  leds.clear();
+}
+
+/**
+ * Animate an array of frames out to our ws2801 led strip
+ * @param  {Array}  frames      Array of frames built in actions/image.js
+ * @param  {Number} frameDelta  Time between each frame (NOTE: ~15ms per frame seems to be the fastest we can animate)
+ * @param  {Number} loop        Animation should continously loop
+ */
+function animate(frames, frameDelta, loop) {
+  var frameCount = 0,
+    curFrame,
+    curFrameLen,
+    i;
+
+  // Set current interval loop up with animation
+  curInterval = setInterval(function () {
+    if (!frames[frameCount]) {
+      if (loop) {
+        frameCount = 0;
+      } else {
+        console.log('END OF ANIMATION');
+        reset();
+        return;
+      }
+    }
+    curFrame = frames[frameCount];
+    curFrameLen = curFrame.length;
+    // Set our animation up
+    for (i = 0; i < curFrameLen; i++) {
+      leds.setColor(i, curFrame[i]);
+    }
+    // Update LEDS
+    leds.update();
+
+    frameCount++;
+  }, frameDelta);
+}
+
+// Runs when ANY topic message we subscribed to gets published
+device.on('message', function (topic, payload) {
+  var rgb, data;
+
+  console.log('message:', topic, payload.toString());
+  // display some number or string (only reads first two characters in string)
+  if (topic === 'blinkie:fill') {
+    reset();
+    try {
+      data = JSON.parse(payload);
+    } catch(e) {
+      console.log('payload JSON is malformed:\n ' + payload);
+      return;
+    }
+    leds.fill(rgb.r, rgb.g, rgb.b);
+  }
+  if (topic === 'blinkie:clear') {
+    reset();
+  }
+  // Gif payload looks like
+  // {
+  //   "imgUrl" : "http://some-gif-url-here",
+  //   "frameDelta: 20, (the milisecond delay you want between each frame)
+  //   "loop": true, (continous play)
+  //   "yOffset": 50 (we only read out a 1px high "slice" thats led)
+  // }
+  if (topic === 'blinkie:gif') {
+    // Try to parse the JSON payload
+    try {
+      data = JSON.parse(payload);
+    } catch(e) {
+      console.log('payload JSON is malformed:\n ' + payload);
+      return;
+    }
+
+
+    // pass off message data to frame builder
+    frameBuilder(data.imgUrl, ledCount, data.yOffset).then(function (frames) {
+      if (!frames) {
+        console.log('Awaiting messages... ');
+      } else {
+        console.log('calling animate with: ' + frames.length + ' frames, delta: ' + data.frameDelta);
+        animate(frames, data.frameDelta, data.loop);
+      }
+    });
+  }
+});
